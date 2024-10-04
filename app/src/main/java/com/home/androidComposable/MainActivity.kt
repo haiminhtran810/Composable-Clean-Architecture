92742337851e1e6842ce488cfba58bb99fb7b5ad
nhoctax <hai.t@belive.sg>
2024-10-05 00:27:58 +0700

    4. Lists and animations

--- app/src/main/java/com/home/androidComposable/MainActivity.kt
+++ app/src/main/java/com/home/androidComposable/MainActivity.kt
@@ -6,33 +6,48 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.home.androidComposable.data.MessageModel
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import com.home.androidComposable.data.fakeData
+import androidx.compose.runtime.setValue
 
-// Material Design được xây dựng dựa trên 3 trụ cột:
-// Color (Màu), Typography (Kiểu chữ) và Shape
+// 1. Composable function: Text, Image...
+// 2. Layout: Column, Row, Modifier.
+// 3. Material Design has 2 main: Color (Màu), Typography (Kiểu chữ) và Shape
+// 4. Lists and Animation
+
+// Unit: Lists and animations are everywhere in apps.
+// In this lesson, you will learn how Compose makes it easy to create lists and fun to add animations. and register events
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Surface {
-                MessageCard(message = MessageModel("Nhoc Tax", " I am Android Developer"))
+                Conversation(messages = fakeData())
             }
         }
     }
@@ -56,23 +71,54 @@ class MainActivity : ComponentActivity() {
                     .border(width = 1.dp, MaterialTheme.colorScheme.primary)
             )
             Spacer(modifier = Modifier.width(5.dp))
-            Column {
-                Row {
-                    Text(text = "Author:")
-                    Text(
-                        text = message.author,
-                        color = MaterialTheme.colorScheme.primary,
-                        style = MaterialTheme.typography.titleSmall
-                    )
-                }
+            // We keep track if the message is expanded or not in this
+            // variable
+            var isExpanded by remember { mutableStateOf(false) }
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.titleSmall
+                )
                 Spacer(modifier = Modifier.width(5.dp))
-                Row {
-                    Text(text = "Body:")
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
                     Text(
                         text = message.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if (isExpanded) {
+                            Int.MAX_VALUE
+                        } else {
+                            1
+                        },
                         color = MaterialTheme.colorScheme.primaryContainer,
                         style = MaterialTheme.typography.bodyMedium
                     )
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun Conversation(messages: List<MessageModel>) {
+        Column {
+            Image(
+                painter = painterResource(id = android.R.drawable.ic_menu_close_clear_cancel),
+                contentDescription = "Action Close",
+                modifier = Modifier
+                    .clickable {
+                        finish()
+                    }
+                    .padding(5.dp, 10.dp)
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            LazyColumn {
+                items(messages) { message ->
+                    MessageCard(message = message)
                 }
             }
         }
@@ -94,4 +140,11 @@ class MainActivity : ComponentActivity() {
             )
         }
     }
+
+    @Preview
+    @Composable
+    fun PreviewConversation() {
+        Conversation(messages = fakeData())
+    }
+
 }
--- app/src/main/java/com/home/androidComposable/data/MessageModel.kt
+++ app/src/main/java/com/home/androidComposable/data/MessageModel.kt
@@ -4,3 +4,10 @@ data class MessageModel(
     val author: String,
     val body: String
 )
+
+fun fakeData() = arrayListOf(
+    MessageModel(author = "Name 1", body = "Hey, take a look at Jetpack Compose, it's great!"),
+    MessageModel(author = "Name 2", body = "Reduces power usage by a significant amount, depending on the device’s screen technology."),
+    MessageModel(author = "Name 3", body = "Hey, take a look at Jetpack Compose, it's great!"),
+    MessageModel(author = "Name 4", body = "Hey, take a look at Jetpack Compose, it's great!"),
+)

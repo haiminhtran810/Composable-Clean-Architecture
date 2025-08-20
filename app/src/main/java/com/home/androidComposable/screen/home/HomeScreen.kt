9c33d215bf38385ceb9f5efa476f07a87565f969
HaiMinhTranBelive <hai.t@belive.sg>
2025-08-20 15:44:32 +0700

    .gitignore is now working

--- app/src/main/java/com/home/androidComposable/screen/home/HomeScreen.kt
+++ app/src/main/java/com/home/androidComposable/screen/home/HomeScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,6 +13,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,7 +25,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.home.androidComposable.R
-import com.home.androidComposable.screen.components.StandardToolbar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.collectAsState
@@ -53,49 +53,19 @@ fun HomeScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenContent(homeUiState: HomeUiState) {
-    val context = LocalContext.current
     val popularMovies = homeUiState.popularMovies
     val topRatedMovies = homeUiState.topRatedMovies
     val upcomingMovies = homeUiState.upcomingMovies
     val nowPlayingMovies = homeUiState.nowPlayingMovies
     Scaffold(
-        topBar = {
-            StandardToolbar(
-                showBackArrow = false,
-                modifier = Modifier.fillMaxWidth(),
-                title = {
-                    Column {
-                        Image(
-                            painterResource(id = R.drawable.pokemon),
-                            contentDescription = null,
-                            modifier =
-                            Modifier
-                                .size(90.dp)
-                                .padding(8.dp)
-                        )
-                    }
-                }
-            )
-        }
-    ) { innnerPadding ->
+        topBar = {}) { innerPadding ->
         PullToRefreshBox(
-            isRefreshing = false,
-            onRefresh = { },
-            modifier = Modifier.padding(innnerPadding)
+            isRefreshing = false, onRefresh = { }, modifier = Modifier.padding(innerPadding)
         ) {
+            val listState = rememberLazyListState() // Optimized state handling
             LazyColumn(
-                contentPadding = PaddingValues(16.dp)
+                state = listState, contentPadding = PaddingValues(16.dp)
             ) {
-//                item {
-//                    FilmCategory(
-//                        items = listOf(
-//                            stringResource(id = R.string.tab_movies),
-//                            stringResource(id = R.string.tab_tv_shows)
-//                        ),
-//                        modifier = Modifier.fillMaxWidth()
-//                    )
-//                }
-
                 item {
                     Spacer(modifier = Modifier.size(16.dp))
                     Column(
@@ -108,8 +78,7 @@ fun HomeScreenContent(homeUiState: HomeUiState) {
                         Spacer(modifier = Modifier.size(8.dp))
                     }
                     PagedRow(
-                        item = popularMovies,
-                        modifier = Modifier.fillMaxWidth()
+                        movies = popularMovies, modifier = Modifier.fillMaxWidth()
                     )
                 }
 
@@ -126,8 +95,7 @@ fun HomeScreenContent(homeUiState: HomeUiState) {
                         Spacer(modifier = Modifier.size(8.dp))
                     }
                     PagedRow(
-                        item = topRatedMovies,
-                        modifier = Modifier.fillMaxWidth()
+                        movies = topRatedMovies, modifier = Modifier.fillMaxWidth()
                     )
                 }
 
@@ -143,8 +111,7 @@ fun HomeScreenContent(homeUiState: HomeUiState) {
                         Spacer(modifier = Modifier.size(8.dp))
                     }
                     PagedRow(
-                        item = upcomingMovies,
-                        modifier = Modifier.fillMaxWidth()
+                        movies = upcomingMovies, modifier = Modifier.fillMaxWidth()
                     )
                 }
 
@@ -160,8 +127,7 @@ fun HomeScreenContent(homeUiState: HomeUiState) {
                         Spacer(modifier = Modifier.size(8.dp))
                     }
                     PagedRow(
-                        item = nowPlayingMovies,
-                        modifier = Modifier.fillMaxWidth()
+                        movies = nowPlayingMovies, modifier = Modifier.fillMaxWidth()
                     )
                     Spacer(modifier = Modifier.size(16.dp))
                 }
@@ -176,12 +142,12 @@ fun FilmCategory(
     modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.Center
+        modifier = modifier, horizontalArrangement = Arrangement.Center
     ) {
         items.forEach { item ->
             Text(
-                text = item, modifier = Modifier.padding(8.dp),
+                text = item,
+                modifier = Modifier.padding(8.dp),
                 style = MaterialTheme.typography.headlineSmall.copy(
                     color = MaterialTheme.colorScheme.onBackground
                 )
@@ -194,15 +160,10 @@ fun FilmCategory(
 
 @Composable
 fun FilmItem(
-    modifier: Modifier = Modifier,
-    imageUrl: String
+    modifier: Modifier = Modifier, imageUrl: String
 ) {
-    // Use Coil lib to load image
     AsyncImage(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(imageUrl)
-            .crossfade(true)
-            .build(),
+        model = ImageRequest.Builder(LocalContext.current).data(imageUrl).crossfade(true).build(),
         placeholder = painterResource(R.drawable.pokemon),
         error = painterResource(id = R.drawable.pokemon),
         contentDescription = null,
@@ -217,32 +178,32 @@ fun FilmItem(
 @Composable
 fun PagedRow(
     modifier: Modifier = Modifier,
-    item: List<Movie>,
+    movies: List<Movie>,
 ) {
     LazyRow {
-        item {
+        items(
+            items = movies, key = {
+                it.id ?: 0
+            }) { _ ->
             Row(
-                modifier = Modifier
-                    .fillMaxWidth()
+                modifier = Modifier.fillMaxWidth()
             ) {
-                item.forEach { movie ->
-
+                movies.forEach { movie ->
                     FilmItem(
                         modifier = Modifier
                             .height(150.dp)
                             .width(100.dp),
                         imageUrl = movie.posterPath ?: ""
                     )
-
                     Spacer(
                         modifier = Modifier
                             .width(5.dp)
                             .height(1.dp)
                     )
                 }
-
             }
         }
+
     }
 }
 
--- app/src/main/java/com/home/androidComposable/screen/home/HomeUiState.kt
+++ app/src/main/java/com/home/androidComposable/screen/home/HomeUiState.kt
@@ -1,11 +1,8 @@
 package com.home.androidComposable.screen.home
 
 import com.home.domain.model.movie.Movie
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 
 data class HomeUiState(
-    // Movie
     val popularMovies: List<Movie> = emptyList(),
     val topRatedMovies: List<Movie> = emptyList(),
     val nowPlayingMovies: List<Movie> = emptyList(),
